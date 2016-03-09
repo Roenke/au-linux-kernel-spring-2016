b83fe48e87540484bf4f5db97c9766ba952c329e
Roenke <roenke54@gmail.com>
2016-03-09 21:06:20 +0300

    Implement stack. Add test. Print processes.

--- tasks/linked_lists/stack.c
+++ tasks/linked_lists/stack.c
@@ -1,28 +1,40 @@
+#include <linux/gfp.h>
+#include <linux/slab.h>
+
+#include "assert.h"
 #include "stack.h"
 
 stack_entry_t* create_stack_entry(void *data)
 {
-    // TODO
-    (void)data;
-    return NULL;
+    stack_entry_t* new_entry = (stack_entry_t*) kmalloc(sizeof(*new_entry), GFP_KERNEL);
+
+    if(new_entry) {
+        INIT_LIST_HEAD(&new_entry->lh);
+        STACK_ENTRY_DATA_RESET(new_entry, data);
+    }
+
+    return new_entry;
 }
 
 void delete_stack_entry(stack_entry_t *entry)
 {
-    // TODO
-    (void)entry;
+    assert(entry);
+    kfree(entry);
 }
 
 void stack_push(struct list_head *stack, stack_entry_t *entry)
 {
-    // TODO
-    (void)stack;
-    (void)entry;
+    assert(stack);
+    assert(entry);
+    list_add(&entry->lh, stack);
 }
 
 stack_entry_t* stack_pop(struct list_head *stack)
 {
-    // TODO
-    (void)stack;
-    return NULL;
+    assert(stack);
+
+    stack_entry_t* entry = (stack_entry_t*)list_first_entry(stack, stack_entry_t, lh);
+    list_del(&entry->lh);
+
+    return entry;
 }
